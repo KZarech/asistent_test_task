f5cc50b9e5a6301a579aaec3fe5bc0ab3617a13c
Kostya <zarechnev.kons@gmail.com>
2024-04-17 18:55:29 +0500

    feat: created file structure, db_migration

---
+++ backend/Users/UserHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+class UserHandler
+{
+    private static string $tableName = "users";
+
+//    public static function createUser() {
+//
+//    }
+
+//    public static function editUser() {
+//
+//    }
+
+//    public static function deleteUser() {
+//
+//    }
+
+//    public static function getUserList() {
+//
+//    }
+}
